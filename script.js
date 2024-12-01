738106786e2a4bc88cefe0ebe7cd7d916f85e9c2
Shrisant Adhikari <Shreesantadhikari4590@gmail.com>
2024-12-01 19:57:15 +0545

    alert feature added

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+function showAlert(){
+    alert("Are you sure want to change the color ");
+}
+
+
 const handleColor=()=>{
+    showAlert();
     document.getElementsByClassName('container')[0].style.backgroundColor='lightgreen';
 }
