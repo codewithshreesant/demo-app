aab14c02f6a7e54d39cdca756f441d7ad689c57e
Shrisant Adhikari <Shreesantadhikari4590@gmail.com>
2024-12-01 20:07:09 +0545

    alert feature while  increase font size

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const handleColor=()=>{
     showAlert();
     document.getElementsByClassName('container')[0].style.backgroundColor='lightgreen';
 }
+
+const IncreaseSize=()=>{
+    document.getElementsByClassName('container')[0].style.fontSize+=10+'px';
+}
+
+
+
